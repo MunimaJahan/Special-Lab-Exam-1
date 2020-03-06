6194846a332d6bc9dd8c8efe95c50d6772d80e7a
MunimaJahan <59153345+MunimaJahan@users.noreply.github.com>
2020-03-05 22:43:28 -0700

    Update Category.java

--- src/spLabExam/Category.java
+++ src/spLabExam/Category.java
@@ -3,16 +3,6 @@ package spLabExam;
 
 public class Category {
 	
-	private String name;
-
-	public Category(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-	
 	
 
 }
