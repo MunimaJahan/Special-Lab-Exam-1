c2e4fb2559f547847a1d924953805422e5027d69
MunimaJahan <59153345+MunimaJahan@users.noreply.github.com>
2020-03-05 22:44:17 -0700

    Update Item.java

--- src/spLabExam/Item.java
+++ src/spLabExam/Item.java
@@ -3,35 +3,5 @@ package spLabExam;
 
 public class Item {
 	
-	private String name;
-	private String itemcode;
-	private Category category;
-	private double unitprice;
-	
-	public Item(String name, 
-			String icode, 
-			Category category,double uprice) {
-		this.name = name;
-		this.itemcode = icode;
-		this.category = category;
-		this.unitprice=uprice;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getItemCode() {
-		return itemcode;
-	}
-	
-	public Category getCategory() {
-		return category;
-	}
-	
-	public double getUnitprice() {
-		return unitprice;
-	}
-	
 
 }
